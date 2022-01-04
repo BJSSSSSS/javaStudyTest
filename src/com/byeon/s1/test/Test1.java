9eff685fbff8051ec11c3b710eb34e8feef7b3c5
BJSSSSSS <john0222223@gmail.com>
2022-01-04 12:22:56 +0900

    final Main

--- src/com/byeon/s1/test/Test1.java
+++ src/com/byeon/s1/test/Test1.java
@@ -3,6 +3,7 @@ package com.byeon.s1.test;
 public class Test1 {
 
 	public static void main(String[] args) {
+		System.out.println("Start");
 		System.out.println("하이");
 		System.out.println("바이");
 		System.out.println("Finish");
